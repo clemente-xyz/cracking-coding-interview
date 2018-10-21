dbdc13341a496f74279c56ad77440075710bf47d
ClementeSerrano <clementeserranosutil@gmail.com>
2018-10-21 18:10:09 -0300

    added super digit problem resolution

--- exercises/others.py
+++ exercises/others.py
@@ -1,17 +1,24 @@
 # Problem 1
 '''
-We add a Leap Day on February 29, almost every four years. The leap day is an extra, or intercalary day and we add it to the shortest month of the year, February.
+We add a Leap Day on February 29, almost every four years. The leap 
+day is an extra, or intercalary day and we add it to the shortest 
+month of the year, February.
 
-In the Gregorian calendar three criteria must be taken into account to identify leap years:
+In the Gregorian calendar three criteria must be taken into account 
+to identify leap years:
 
 1. The year can be evenly divided by 4, is a leap year, unless:
-1.1. The year can be evenly divided by 100, it is NOT a leap year, unless:
-1.1.1. The year is also evenly divisible by 400. Then it is a leap year.
+1.1. The year can be evenly divided by 100, it is NOT a leap year, 
+    unless: 1.1.1. The year is also evenly divisible by 400. Then 
+    it is a leap year.
 
-This means that in the Gregorian calendar, the years 2000 and 2400 are leap years, while 1800, 1900, 2100, 2200, 2300 and 2500 are NOT leap years.
+This means that in the Gregorian calendar, the years 2000 and 2400 
+are leap years, while 1800, 1900, 2100, 2200, 2300 and 2500 are NOT 
+leap years.
 
 Task:
-You are given the year, and you have to write a function to check if the year is leap or not.
+You are given the year, and you have to write a function to check 
+if the year is leap or not.
 
 Constraints:
 1900<=y<=10^5
@@ -50,10 +57,43 @@ Output the answer as explained in the task.
 
 def generate_secuence(n):
     aux_array = []
+
     for i in range(1, n+1):
         aux_array.append(str(i))
+
     return ''.join(aux_array)
 
 
-A = [3, 4, 7, 7, 6, 6]
-print(generate_secuence(20))
+# Problem 3
+'''
+Given an integer x, the super digit of the integer is defined as:
+
+- If x has only 1 digit, then its super digit is x.
+- Otherwise, the super digit of x is equal to the super digit of 
+  the sum of the digits of x.
+
+Given two numbers: n and k, write a function to calculate the super 
+digit of the number p created by concatenating the number n k times.
+'''
+
+
+def super_digit(n, k):
+    _n = str(n)
+    p = _n*k
+
+    return _super_digit(p)
+
+
+def _super_digit(p):
+    if len(p) == 1:
+        return p
+
+    j = 0
+
+    for i in p:
+        j += int(i)
+
+    return _super_digit(str(j))
+
+
+print(super_digit(9875, 4))
